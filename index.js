9eebc693afda3a53adaccf7fa7feaf8e3cb98e78
8ctopotamus <joshnaylor88@gmail.com>
2018-02-13 16:43:33 -0600

    added Screenshot generated msg and screenshots directory

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   page.setViewport({width: 1200, height: 2000})
   await page.goto('https://realwealthmedia.com/jadeweekly',{"waitUntil" : "networkidle0"});
-  await page.screenshot({path: 'jadeweekly.png'});
+  await page.screenshot({path: 'screenshots/jadeweekly.jpg'});
   await browser.close();
+  console.log('Your screenshot has been generated. 👍')
+  console.log('You can find it in /screenshots.')
 })();
